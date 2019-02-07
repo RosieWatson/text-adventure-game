80803f051554e36a6da47ff409ad9587bfc732a0
Rosie Watson <rosie.watson@holidayextras.com>
2019-02-07 18:24:57 +0000

    Tidying up html

--- script.js
+++ script.js
@@ -15,35 +15,35 @@ window.onload = function() {
 
 // Shows a welcome message to the user
 function generateWelcomeMessage() {
-  let output = document.getElementById("output").innerHTML
-  output = "Hello player! Please enter your character name:"
+  let output = document.getElementById("output")
+  output.innerHTML = "Hello player! Please enter your character name:"
 }
 
 function yourAnswer() {
     let answer = document.getElementById("answer").value
-    let output = document.getElementById("output").innerHTML
+    let output = document.getElementById("output")
     let correctAnswer = answers[qustionNumber]
 
     if(name === undefined) {
         name = answer
-        output =  output + "</br></br>" + "Welcome " + name + "! You have " + health + " health."
-        output =  output + "</br></br>" + "You enter the courtyard and look around. You are surrounded by a high wall and there is a door at the end of the yard. A man gaurds the door. Do you want to fight the name or sneak round."
-        setUpInfo(name)
+        output.innerHTML =  output.innerHTML + "</br></br>" + "Welcome " + name + "! You have " + health + " health."
+        output.innerHTML =  output.innerHTML + "</br></br>" + "You enter the courtyard and look around. You are surrounded by a high wall and there is a door at the end of the yard. A man gaurds the door. Do you want to fight the name or sneak round."
+        setUpInfo()
     } else if(inQuestion && answer.toLowerCase() === correctAnswer) {
-        output =  output + "</br></br>" + "You beat the man in a fight!"
+        output.innerHTML =  output.innerHTML + "</br></br>" + "You beat the man in a fight!"
         qustionNumber++
         inQuestion = false
     } else if(inQuestion && answer.toLowerCase() !== correctAnswer) {
-        output =  output + "</br></br>" + "You tried to fight the man but he is too strong..."
+        output.innerHTML =  output.innerHTML + "</br></br>" + "You tried to fight the man but he is too strong..."
         health = health - 20
-        inQuestion = false
         setUpInfo()
+        inQuestion = false
     } else if (answer.toLowerCase() === 'fight') {
       inQuestion = true
-      output =  output + "</br></br>" + "You have chosen to fight the man. You must answer a question correctly to beat him."
-      output =  output + "</br></br>" + questions[qustionNumber]
+      output.innerHTML =  output.innerHTML + "</br></br>" + "You have chosen to fight the man. You must answer a question correctly to beat him."
+      output.innerHTML =  output.innerHTML + "</br></br>" + questions[qustionNumber]
     } else if (answer.toLowerCase() === 'sneak') {
-      output =  output + "</br></br>" + "You have snuck around the man."
+      output.innerHTML =  output.innerHTML + "</br></br>" + "You have snuck around the man."
     } else {
       alert('Sorry I do not understand your answer, please try again!')
     }
@@ -55,5 +55,3 @@ function setUpInfo() {
   document.getElementById("name").innerHTML = "Name: " + name
   document.getElementById("health").innerHTML = "Health: " + health
 }
-
-
