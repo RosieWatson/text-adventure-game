a81017b282667a9bbcf5efb7477d6c0c4d7fa691
Rosie Watson <rosie.watson@holidayextras.com>
2019-02-08 13:41:06 +0000

    Adding in rounds

--- script.js
+++ script.js
@@ -5,8 +5,37 @@ let health = 100
 // Question variables
 let inQuestion = false
 let qustionNumber = 0
-let questions = ["What is the capital of England?", "What colour is an apple?", "How many wives did Henry VIII have?"]
-let answers = ["london", "red", "6"]
+let roundNumber = 0
+let descriptions = [
+  "You enter the courtyard and look around. You are surrounded by a high wall and there is a door at the end of the yard. A man guards the door. Do you want to fight the man or sneak round?",
+  "There is a snake in your path, do you want to befriend or ignore him?",
+  "The castle is surrounded by a moat. Do you jump the moat or turn around?"
+]
+let actions = [
+  "You have chosen to fight the man. You must answer a question correctly to beat him.",
+  "You have chosen to befriend the snake. To be his friend you have to answer this question.",
+  "You have chosen to jump the moat, to make it across you must answer this question."
+]
+let failMessages = [
+  "You tried to fight the man but he is too strong...",
+  "You were wrong, the snake bites your foot!",
+  "You fell in the water! Get out and try again."
+]
+let winMessage = [
+  "You beat the man in a fight!",
+  "The snake is joining you for this quest!",
+  "You made it across!"
+]
+let questions = [
+  "What is the capital of England?",
+  "What colour is an apple?",
+  "How many wives did Henry VIII have?"
+]
+let answers = [
+  "london",
+  "red",
+  "6"
+]
 
 // When the browser widow loads it will call the generateWelcomeMessage() function
 window.onload = function() {
@@ -26,11 +55,11 @@ function inputText() {
     if(name === undefined) {
         name = answer
         output.innerHTML =  output.innerHTML + "</br></br>" + "Welcome " + name + "! You have " + health + " health."
-        output.innerHTML =  output.innerHTML + "</br></br>" + "You enter the courtyard and look around. You are surrounded by a high wall and there is a door at the end of the yard. A man guards the door. Do you want to fight the man or sneak round."
+        output.innerHTML =  output.innerHTML + "</br></br>" + descriptions[roundNumber]
         setUpInfo()
     } else if(inQuestion) {
       questionAnswered(answer)
-    } else if (answer.toLowerCase() === 'fight') {
+    } else if (answer.toLowerCase() === 'fight' || answer.toLowerCase() === 'befriend' || answer.toLowerCase() === 'jump') {
       askQuestion()
     } else if (answer.toLowerCase() === 'sneak' || answer.toLowerCase() === 'exit') {
       output.innerHTML =  output.innerHTML + "</br></br>" + "Thank you for playing."
@@ -51,21 +80,39 @@ function questionAnswered(answer) {
   let correctAnswer = answers[qustionNumber]
 
   if (answer.toLowerCase() === correctAnswer) {
-    output.innerHTML =  output.innerHTML + "</br></br>" + "You beat the man in a fight!"
-    qustionNumber++
+    output.innerHTML =  output.innerHTML + "</br></br>" + winMessage[roundNumber]
     inQuestion = false
+    nextRound()
   } else if(answer.toLowerCase() !== correctAnswer && health > 0) {
-    output.innerHTML =  output.innerHTML + "</br></br>" + "You tried to fight the man but he is too strong..."
+    output.innerHTML =  output.innerHTML + "</br></br>" + failMessages[roundNumber]
     health = health - 20
     setUpInfo()
   } else {
     // maybe out of question and show end screen?
   }
+
+  qustionNumber++
 }
 
 // Prints out the next question for the user
 function askQuestion() {
   inQuestion = true
-  output.innerHTML =  output.innerHTML + "</br></br>" + "You have chosen to fight the man. You must answer a question correctly to beat him."
+  output.innerHTML =  output.innerHTML + "</br></br>" + actions[roundNumber]
   output.innerHTML =  output.innerHTML + "</br></br>" + questions[qustionNumber]
+}
+
+// Checks if the user has won or if it needs to load the next round
+function nextRound() {
+  roundNumber = roundNumber + 1
+
+  if (roundNumber > 2) {
+    winningSequence()
+  } else {
+    output.innerHTML =  output.innerHTML + "</br></br>" + descriptions[roundNumber]
+  }
+}
+
+// Prints the winning message for the user
+function winningSequence() {
+  output.innerHTML =  output.innerHTML + "</br></br>" + "Congratulations you have beat the quest and made it into the castle!"
 }
